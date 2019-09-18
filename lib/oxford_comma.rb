dd57c1af57375739a7142f0b96e4e36f750fdc3d
James Benjamin <james.abenj@gmail.com>
2019-09-18 03:28:11 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
 new_string = ""
   array.each do |element|
    if array.size == 1 || element == array[0] 
-    new_string << "#{element}"
+    new_string << "#{element} "
   elsif
   array.size > 1 && element != array[-1]
     new_string << "#{element}, "
